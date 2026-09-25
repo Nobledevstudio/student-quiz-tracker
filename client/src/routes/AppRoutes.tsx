2ee59257ea9af592c704be767342c94095edc869
Atilola Victor <atilolavictor4562@gmail.com>
2026-09-25 07:09:57 -0700

    feat(routing): configure application routes

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,20 +1,54 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import DashboardPage from "../pages/DashboardPage";
+import AppLayout from "../components/layout/AppLayout";
+import CreateQuizPage from "@/pages/CreateQuizPage";
+import DashboardPage from "@/pages/DashboardPage";
+import QuizzesPage from "@/pages/QuizzesPage";
+import QuizResultPage from "@/pages/QuizResultPage";
+import ResultsHistoryPage from "@/pages/ResultsHistoryPage";
+import TakeQuizPage from "@/pages/TakeQuizPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<Navigate to="/dashboard" replace />}
-      />
+      <Route element={<AppLayout />}>
+        <Route
+          path="/"
+          element={<Navigate to="/dashboard" replace />}
+        />
 
-      <Route
-        path="/dashboard"
-        element={<DashboardPage />}
-      />
+        <Route
+          path="/dashboard"
+          element={<DashboardPage />}
+        />
+
+        <Route
+          path="/quizzes"
+          element={<QuizzesPage />}
+        />
+
+        <Route
+          path="/quizzes/:id/results"
+          element={<ResultsHistoryPage />}
+        />
+
+        <Route
+          path="/quizzes/:id/result"
+          element={<QuizResultPage />}
+        />
+
+        <Route
+          path="/quizzes/:id"
+          element={<TakeQuizPage />}
+        />
+
+        <Route
+          path="/create-quiz"
+          element={<CreateQuizPage />}
+        />
+      </Route>
     </Routes>
   );
 };
 
 export default AppRoutes;
+
